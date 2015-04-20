43c296402b41d76b5709d46610948d5bba6ba36c
allejo <allejo@me.com>
2015-04-20 11:23:14 -0700

    Rename everything + add CSS file

--- jquery.jumbotube.js
+++ jquery.jumbotube.js
@@ -28,11 +28,11 @@
         repeat: true,
         width: $(window).width(),
         wrapperZIndex: 99,
-        playButtonClass: 'tubular-play',
-        pauseButtonClass: 'tubular-pause',
-        muteButtonClass: 'tubular-mute',
-        volumeUpClass: 'tubular-volume-up',
-        volumeDownClass: 'tubular-volume-down',
+        playButtonClass: 'jumbotube-play',
+        pauseButtonClass: 'jumbotube-pause',
+        muteButtonClass: 'jumbotube-mute',
+        volumeUpClass: 'jumbotube-volume-up',
+        volumeDownClass: 'jumbotube-volume-down',
         volumeChangeFactor: 10,
         start: 0,
         end: -1,
@@ -50,9 +50,9 @@
             $node = $(node); // cache wrapper node
 
         // build container
-        var jumbotubeContainer = '<div id="tubular-container" style="overflow: hidden; position: fixed; z-index: 1; width: 100%; height: 100%"><div id="tubular-player" style="position: absolute"></div></div><div id="tubular-shield" style="width: 100%; height: 100%; z-index: 2; position: absolute; left: 0; top: 0;"></div>';
+        var jumbotubeContainer = '<div id="jumbotube-container" class="jumbotube-container"><div id="jumbotube-player" class="jumbotube-player"></div></div><div id="jumbotube-shield" class="jumbotube-shield"></div>';
 
-        // set up css prereq's, inject tubular container and set up wrapper defaults
+        // set up css prereq's, inject jumbotube container and set up wrapper defaults
         $('html,body').css({ 'width': '100%', 'height': '100%' });
         $body.prepend(jumbotubeContainer);
         $node.css({ position: 'relative', 'z-index': options.wrapperZIndex });
@@ -60,7 +60,7 @@
         // set up iframe player, use global scope so YT api can talk
         window.JumboPlayer = null;
         window.onYouTubeIframeAPIReady = function () {
-            JumboPlayer = new YT.Player('tubular-player', {
+            JumboPlayer = new YT.Player('jumbotube-player', {
                 width: options.width,
                 height: Math.ceil(options.width / options.ratio),
                 videoId: options.videoID,
@@ -123,16 +123,16 @@
                 pWidth, // player width, to be defined
                 height = $(window).height(),
                 pHeight, // player height, tbd
-                $tubularPlayer = $('#tubular-player');
+                $jumbotubePlayer = $('#jumbotube-player');
 
             // when screen aspect ratio differs from video, video must center and underlay one dimension
 
             if (width / options.ratio < height) { // if new video height < window height (gap underneath)
                 pWidth = Math.ceil(height * options.ratio); // get new player width
-                $tubularPlayer.width(pWidth).height(height).css({ left: (width - pWidth) / 2, top: 0 }); // player width is greater, offset left; reset top
+                $jumbotubePlayer.width(pWidth).height(height).css({ left: (width - pWidth) / 2, top: 0 }); // player width is greater, offset left; reset top
             } else { // new video width < window width (gap to right)
                 pHeight = Math.ceil(width / options.ratio); // get new player height
-                $tubularPlayer.width(width).height(pHeight).css({ left: 0, top: (height - pHeight) / 2 }); // player height is greater, offset top; reset left
+                $jumbotubePlayer.width(width).height(pHeight).css({ left: 0, top: (height - pHeight) / 2 }); // player height is greater, offset top; reset left
             }
         };
 
@@ -212,8 +212,8 @@
 
     $.fn.jumbotube = function (options) {
         return this.each(function () {
-            if (!$.data(this, 'tubular_instantiated')) { // let's only run one
-                $.data(this, 'tubular_instantiated',
+            if (!$.data(this, 'jumbotube_instantiated')) { // let's only run one
+                $.data(this, 'jumbotube_instantiated',
                 jumbotube(this, options));
             }
         });
